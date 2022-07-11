451c58161b60adefc5b9ae77a389b4a1aef339d0
Mitchell Borrego <mitchell@codecov.io>
2022-07-11 13:58:54 -0400

    Replace `toBe` with `toBeCloseTo`

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,6 +6,6 @@ describe(`${toSeconds.name}()`, () => {
   it.each([
     { ms: 5_400, expected: 5.4 },
   ])("converts %n to %n", ({ ms, expected }) => {
-    expect(toSeconds(ms)).toBe(expected);
+    expect(toSeconds(ms)).toBeCloseTo(expected);
   });
 });

1d45a425a08b317fc65fbe0e933c0d48f51c6d1e
Mitchell Borrego <mitchell@codecov.io>
2022-07-14 05:58:25 -0400

    Run `npm install`

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ HEAD="$(git branch --show-current 2> /dev/null)"
 
 [ "$(cat HEAD 2> /dev/null)" = "${HEAD}" ] && exit 0
 
+npm install || exit "${?}"
+
 echo "${HEAD}" > HEAD 2> /dev/null || {
     printf "%s: %s\n" "${0}" "Couldn't save the current branch to disk"
     exit 1
